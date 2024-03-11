e892a8843bb24627e85835c45d271c04b05797ab
ritdsn <ritchellifox@gmail.com>
2024-03-11 20:30:44 -0300

    inserindo um novo Commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá Galera, ínicio de tudo aqui!");
+        System.out.println("Teste de outro Commit");
     }
 }
